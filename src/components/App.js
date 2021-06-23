d18ec3b1ced47882af3b2b18e3ba275063cb3372
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-22 20:14:41 -0500

    added initial files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,1 @@
-import Button from '@material-ui/core/Button';
-
-function App() {
-    return (
-        <Button
-            onClick={() => alert('welcome captain Gary')}
-            variant="contained"
-            color="primary"
-        >
-            start ship
-        </Button>
-    );
-}
-
-export default App;
+/* App component */
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,8 +1,12 @@
+import sarahedo from './profile01.jpg';
+import tylermcginnis from './profile02.jpg';
+import johndoe from './johndoe.jpg';
+
 let users = {
     sarahedo: {
         id: 'sarahedo',
         name: 'Sarah Edo',
-        avatarURL: '',
+        avatarURL: sarahedo,
         answers: {
             '8xf0y6ziyjabvozdd253nd': 'optionOne',
             '6ni6ok3ym7mf1p33lnez': 'optionTwo',
@@ -14,7 +18,7 @@ let users = {
     tylermcginnis: {
         id: 'tylermcginnis',
         name: 'Tyler McGinnis',
-        avatarURL: '',
+        avatarURL: tylermcginnis,
         answers: {
             vthrdm985a262al8qx3do: 'optionOne',
             xj352vofupe1dqz9emx13r: 'optionTwo',
@@ -24,7 +28,7 @@ let users = {
     johndoe: {
         id: 'johndoe',
         name: 'John Doe',
-        avatarURL: '',
+        avatarURL: johndoe,
         answers: {
             xj352vofupe1dqz9emx13r: 'optionOne',
             vthrdm985a262al8qx3do: 'optionTwo',
