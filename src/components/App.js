5bde4e016c6df0311f06a8a6379c2705857a54ba
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-23 20:28:56 -0500

    fixed always appearing 404 page with Switch

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import { LeaderBoard } from './LeaderBoard';
 import { Login } from './Login';
 import { LogOut } from './Logout';
 import { NotFound } from './404';
+import { Switch } from 'react-router-dom';
 
 class App extends Component {
     state = {};
@@ -20,21 +21,23 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Route exact path="/home" component={Dashboard}></Route>
-                <Route exact path="/add" component={NewQuestion}></Route>
-                <Route
-                    exact
-                    path="/question/:id"
-                    component={QuestionPage}
-                ></Route>
-                <Route
-                    exact
-                    path="/leaderboard"
-                    component={LeaderBoard}
-                ></Route>
-                <Route exact path="/login" component={Login}></Route>
-                <Route exact path="/logout" component={LogOut}></Route>
-                <Route component={NotFound} />
+                <Switch>
+                    <Route exact path="/home" component={Dashboard}></Route>
+                    <Route exact path="/add" component={NewQuestion}></Route>
+                    <Route
+                        exact
+                        path="/question/:id"
+                        component={QuestionPage}
+                    ></Route>
+                    <Route
+                        exact
+                        path="/leaderboard"
+                        component={LeaderBoard}
+                    ></Route>
+                    <Route exact path="/login" component={Login}></Route>
+                    <Route exact path="/logout" component={LogOut}></Route>
+                    <Route component={NotFound} />
+                </Switch>
             </div>
         );
     }
