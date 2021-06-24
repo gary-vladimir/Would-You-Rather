61c82086782986f8636d58d27d94838d40f89b3d
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-23 20:14:39 -0500

    added Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 /* App component */
 import { Component } from 'react';
-
 import { handleInitialData } from '../actions';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Dashboard } from './Dashboard';
+import { NewQuestion } from './NewQuestion';
+import { QuestionPage } from './QuestionPage';
+import { LeaderBoard } from './LeaderBoard';
+import { Login } from './Login';
+import { LogOut } from './Logout';
+import { NotFound } from './404';
 
 class App extends Component {
     state = {};
@@ -11,7 +18,25 @@ class App extends Component {
         this.props.dispatch(handleInitialData());
     }
     render() {
-        return <div>hola gary el pro</div>;
+        return (
+            <div>
+                <Route exact path="/home" component={Dashboard}></Route>
+                <Route exact path="/add" component={NewQuestion}></Route>
+                <Route
+                    exact
+                    path="/question/:id"
+                    component={QuestionPage}
+                ></Route>
+                <Route
+                    exact
+                    path="/leaderboard"
+                    component={LeaderBoard}
+                ></Route>
+                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/logout" component={LogOut}></Route>
+                <Route component={NotFound} />
+            </div>
+        );
     }
 }
 
