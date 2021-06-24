362372dfd7a6797980ca1b1198230edaa9f89f96
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-24 13:41:12 -0500

    navBar with Material-UI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,42 @@
-/* nav bar component */
-import { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { Link } from 'react-router-dom';
 
-export class NavBar extends Component {
-    render() {
-        return (
-            <nav>
-                <NavLink
-                    className="navItem"
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        color: 'white',
+        background: '#16191b',
+        height: '70px',
+    },
+    navitem: {
+        height: '70px',
+    },
+});
+
+export function NavBar() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Paper className={classes.root}>
+            <Tabs value={value} onChange={handleChange}>
+                <Tab
+                    label="Dashboard"
+                    component={Link}
                     to="/home"
-                    activeClassName="active"
-                >
-                    Dashboard
-                </NavLink>
-                <NavLink className="navItem" to="/add" activeClassName="active">
-                    Create Question
-                </NavLink>
-                <NavLink
-                    className="navItem"
-                    to="/leaderboard"
-                    activeClassName="active"
-                >
-                    Leader Board
-                </NavLink>
-            </nav>
-        );
-    }
+                    className={classes.navitem}
+                />
+                <Tab label="Create Question" component={Link} to="/add" />
+                <Tab label="Leaderboard" component={Link} to="/leaderboard" />
+            </Tabs>
+        </Paper>
+    );
 }
