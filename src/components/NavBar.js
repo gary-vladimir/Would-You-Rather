908474892f26269d2909835b2d54ce932deba517
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-24 20:56:23 -0500

    added avatar and logOut btn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
@@ -14,6 +17,18 @@ const useStyles = makeStyles({
     },
     navitem: {
         height: '70px',
+    },
+    logout: {
+        padding: '0',
+        color: 'white',
+        minWidth: '0',
+    },
+    name: {
+        lineHeight: '40px',
+        marginRight: '10px',
+        fontFamily: 'Roboto',
+        fontWeight: '500',
+        color: '#ced4da',
     },
 });
 
@@ -41,6 +56,32 @@ export function NavBar() {
                 <Tab label="Create Question" component={Link} to="/add" />
                 <Tab label="Leaderboard" component={Link} to="/leaderboard" />
             </Tabs>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '13px',
+                    right: '25px',
+                    display: 'flex',
+                    gap: '10px',
+                    flexDirection: 'row-reverse',
+                }}
+            >
+                <Button
+                    className={classes.logout}
+                    component={Link}
+                    to="/logout"
+                >
+                    <ExitToAppIcon style={{ fontSize: '40px' }}></ExitToAppIcon>
+                </Button>
+                <Avatar
+                    style={{
+                        backgroundColor: '#0077b6',
+                    }}
+                >
+                    G
+                </Avatar>
+                <div className={classes.name}>Gary Nuñez</div>
+            </div>
         </Paper>
     );
 }
