43464f54be0ff2c654c08e48b60be551f711c62c
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-26 15:20:26 -0500

    added styles and button from Material-Ui

--- src/components/404.js
+++ src/components/404.js
@@ -1,13 +1,42 @@
 /* component when page not found */
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 
 export class NotFound extends Component {
     render() {
         return (
-            <div>
-                404- sorry it looks like you got lost
-                <Link to="/home">Go Home</Link>
+            <div
+                style={{
+                    color: '#e63946',
+                    fontFamily: 'Montserrat',
+                    textAlign: 'center',
+                    position: 'absolute',
+                    left: '50%',
+                    top: '40%',
+                    transform: 'translate(-50%,-50%)',
+                }}
+            >
+                <h1
+                    style={{
+                        marginTop: '0px',
+                        fontSize: '100px',
+                        marginBottom: '0px',
+                    }}
+                >
+                    404
+                </h1>
+                <h6 style={{ fontSize: '20px', marginTop: '10px' }}>
+                    Page not found
+                </h6>
+                <Button
+                    component={Link}
+                    to="/home"
+                    variant="contained"
+                    color="secondary"
+                >
+                    Go Home
+                </Button>
             </div>
         );
     }
