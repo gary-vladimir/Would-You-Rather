42787d5792921422f8d75558033a04a20681d3a1
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-25 21:17:52 -0500

    added submit button and changed to primary theme for the form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -23,6 +24,8 @@ const useStyles = makeStyles((theme) => ({
         top: '40%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
+        display: 'flex',
+        flexDirection: 'column',
     },
     select: {
         color: 'white',
@@ -38,15 +41,18 @@ export function Login() {
 
     const handleChange = (event) => {
         setUser(event.target.value);
-        console.log(user);
     };
+    console.log(user);
 
     return (
         <div className={classes.title}>
             <h1 style={{ marginBottom: '-5px' }}>Please Log-In</h1>
             <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label" color="secondary">
-                    Age
+                <InputLabel
+                    id="demo-simple-select-label"
+                    style={{ color: 'white' }}
+                >
+                    User
                 </InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
@@ -54,7 +60,7 @@ export function Login() {
                     value={user}
                     onChange={handleChange}
                     className={classes.select}
-                    color="secondary"
+                    color="primary"
                 >
                     <MenuItem classes={classes.item} value={'sarah'}>
                         Sarah
@@ -63,6 +69,9 @@ export function Login() {
                     <MenuItem value={'john'}>John</MenuItem>
                 </Select>
             </FormControl>
+            <Button variant="contained" color="primary">
+                Submit
+            </Button>
         </div>
     );
 }
