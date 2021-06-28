fbf3eb61161bee4fb5d016dc57cac17dc9523cdb
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-28 17:15:51 -0500

    render login if ther is no selected user

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 /* App component */
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { handleInitialData } from '../actions';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Dashboard } from './Dashboard';
 import { NewQuestion } from './NewQuestion';
 import { QuestionPage } from './QuestionPage';
-import { LeaderBoard } from './LeaderBoard';
+import LeaderBoard from './LeaderBoard';
 import { Login } from './Login';
 import { LogOut } from './Logout';
 import { NotFound } from './404';
@@ -22,31 +22,61 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Route
-                    path={['/home', '/add', '/question', '/leaderboard']}
-                    component={NavBar}
-                />
-                <Switch>
-                    <Route exact path="/home" component={Dashboard}></Route>
-                    <Route exact path="/add" component={NewQuestion}></Route>
-                    <Route
-                        exact
-                        path="/question/:id"
-                        component={QuestionPage}
-                    ></Route>
-                    <Route
-                        exact
-                        path="/leaderboard"
-                        component={LeaderBoard}
-                    ></Route>
-                    <Route exact path="/login" component={Login}></Route>
-                    <Route exact path="/logout" component={LogOut}></Route>
+                {this.props.userIn === true ? (
+                    <Route path="/" component={Login}></Route>
+                ) : (
+                    <React.Fragment>
+                        <Route
+                            path={[
+                                '/home',
+                                '/add',
+                                '/question',
+                                '/leaderboard',
+                            ]}
+                            component={NavBar}
+                        />
+                        <Switch>
+                            <Route
+                                exact
+                                path="/home"
+                                component={Dashboard}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/add"
+                                component={NewQuestion}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/question/:id"
+                                component={QuestionPage}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/leaderboard"
+                                component={LeaderBoard}
+                            ></Route>
 
-                    <Route component={NotFound} />
-                </Switch>
+                            <Route
+                                exact
+                                path="/logout"
+                                component={LogOut}
+                            ></Route>
+
+                            <Redirect exact from="/" to="/home" />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </React.Fragment>
+                )}
             </div>
         );
     }
 }
 
-export default connect(null)(App);
+function mapStateToProps({ selectUser }) {
+    return {
+        userIn: selectUser === null,
+    };
+}
+
+export default connect(mapStateToProps)(App);
