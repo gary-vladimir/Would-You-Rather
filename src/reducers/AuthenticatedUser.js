f40f39f51dd35652fe9d3c553c44b88e65de4fc6
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-28 21:34:30 -0500

    added case tto set authed user to null

--- src/reducers/AuthenticatedUser.js
+++ src/reducers/AuthenticatedUser.js
@@ -1,5 +1,6 @@
 /* Authenticated User reducer */
 import { SELECT_USER } from '../actions/AuthenticatedUser';
+import { SET_AUTHED_USER_TO_NULL } from '../actions/AuthenticatedUser';
 
 export default function selectUser(state = null, action) {
     switch (action.type) {
@@ -14,7 +15,8 @@ export default function selectUser(state = null, action) {
                 action
             );
             return action.user;
-
+        case SET_AUTHED_USER_TO_NULL:
+            return null;
         default:
             return state;
     }
