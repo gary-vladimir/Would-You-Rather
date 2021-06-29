397660b5a95f2abd44d1ebcf0dfc63ef19b1a198
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-28 19:48:28 -0500

    added connect react-redux

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Card } from '@material-ui/core';
 import { Avatar } from '@material-ui/core';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -115,7 +116,7 @@ function UserCard(classes, name, position, answeredQ, askedQ, color) {
     );
 }
 
-export function LeaderBoard() {
+function LeaderBoard() {
     const classes = useStyles();
     return (
         <div className={classes.root}>
@@ -127,3 +128,5 @@ export function LeaderBoard() {
         </div>
     );
 }
+
+export default connect(null)(LeaderBoard);
