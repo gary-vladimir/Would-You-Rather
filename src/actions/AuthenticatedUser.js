34baea2f5db0378f24aad784d28517fc25b91b2d
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-28 21:35:47 -0500

    added action creator to logout

--- src/actions/AuthenticatedUser.js
+++ src/actions/AuthenticatedUser.js
@@ -1,9 +1,17 @@
 /* action for authenticated user */
 export const SELECT_USER = 'SELECT_USER';
+export const SET_AUTHED_USER_TO_NULL = 'SET_AUTHED_USER_TO_NULL';
 
 export function selectUser(user) {
     return {
         type: SELECT_USER,
         user: user,
+    };
+}
+
+export function logoutUser(id) {
+    return {
+        type: SET_AUTHED_USER_TO_NULL,
+        id,
     };
 }
