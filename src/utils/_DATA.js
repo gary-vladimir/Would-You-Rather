2229e45b8d9d0c0dcadb4a35e083a51680272af5
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-30 14:33:34 -0500

    added correct profile pictures

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,10 +1,8 @@
-import userImage from './api.jpeg';
-
 let users = {
     sarahedo: {
         id: 'sarahedo',
         name: 'Sarah Edo',
-        avatarURL: userImage,
+        avatarURL: './profile01.jpg',
         answers: {
             '8xf0y6ziyjabvozdd253nd': 'optionOne',
             '6ni6ok3ym7mf1p33lnez': 'optionTwo',
@@ -16,7 +14,7 @@ let users = {
     tylermcginnis: {
         id: 'tylermcginnis',
         name: 'Tyler McGinnis',
-        avatarURL: userImage,
+        avatarURL: './profile02.jpg',
         answers: {
             vthrdm985a262al8qx3do: 'optionOne',
             xj352vofupe1dqz9emx13r: 'optionTwo',
@@ -26,7 +24,7 @@ let users = {
     johndoe: {
         id: 'johndoe',
         name: 'John Doe',
-        avatarURL: userImage,
+        avatarURL: './profile03.jpg',
         answers: {
             xj352vofupe1dqz9emx13r: 'optionOne',
             vthrdm985a262al8qx3do: 'optionTwo',
