ffe16f0f3706fe7b261d57835f6c5395b10e5159
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-30 14:33:59 -0500

    remove log

--- src/reducers/AuthenticatedUser.js
+++ src/reducers/AuthenticatedUser.js
@@ -5,16 +5,10 @@ import { SET_AUTHED_USER_TO_NULL } from '../actions/AuthenticatedUser';
 export default function selectUser(state = null, action) {
     switch (action.type) {
         case SELECT_USER:
-            console.log(
-                'reducer SELECT_USER',
-                'action.user:',
-                action.user,
-                'state:',
-                state,
-                'action:',
-                action
-            );
-            return action.user;
+            return {
+                ...state,
+                id: action.id,
+            };
         case SET_AUTHED_USER_TO_NULL:
             return null;
         default:
